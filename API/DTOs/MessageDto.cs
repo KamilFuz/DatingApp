4f8f8fd01d8acb4e7cc5b6fce7d1b191e347001f
Kamil Fuz <kamf@netcompany.com>
2022-01-03 14:11:49 +0100

    refactors: db queries, use unitOfWork for repositories, confirmation service, scroll for messages

--- API/DTOs/MessageDto.cs
+++ API/DTOs/MessageDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace API.DTOs
 {
@@ -8,11 +9,17 @@ namespace API.DTOs
         public int SenderId { get; set; }
         public string SenderUsername { get; set; }
         public string SenderPhotoUrl { get; set; }
-        public int ReceipientId { get; set; }
-        public string ReceipientUsername { get; set; }
+        public int RecipientId { get; set; }
+        public string RecipientUsername { get; set; }
         public string RecipientPhotoUrl { get; set; }
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }
-        public DateTime? MessageSent { get; set; }
+        public DateTime MessageSent { get; set; }
+
+        // [JsonIgnore]
+        public bool SenderDeleted { get; set; }
+
+        // [JsonIgnore]
+        public bool ReceipientDeteled { get; set; }
     }
 }
--- API/Extensions/ApplicationServiceExternsions.cs
+++ API/Extensions/ApplicationServiceExternsions.cs
@@ -22,9 +22,10 @@ namespace API.Extensions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<ITokenService, TokenService>();
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<ILikesRepository, LikesRepository>();
-            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            // services.AddScoped<IUserRepository, UserRepository>();
+            // services.AddScoped<ILikesRepository, LikesRepository>();
+            // services.AddScoped<IMessageRepository, MessageRepository>();
             services.AddScoped<LogUserActivity>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
